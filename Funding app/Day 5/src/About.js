295d60249866e18a2aaae01e9b721e75690912cf
SanchanaRamesh <110534139+SanchanaRamesh@users.noreply.github.com>
2023-09-21 15:20:53 +0530

    Add files via upload

---
+++ Funding app/Day 5/src/About.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import './About.css'
+const About = () => {
+    
+  return (
+    <div>
+    <>
+    <section className="content-page">
+      <div className="container-fluid">
+        <section className="hero">
+          <div className="container">
+            <div className="row">
+              <h1 className="text-uppercase">Hello, there!!</h1>
+              <span className="text">
+                <p>
+                   
+                </p>
+                <p>We hope your experience is better than ours was.</p>
+              </span>
+            </div>
+          </div>
+        </section>
+        <section className="mission">
+          <div className="container">
+            <div className="row">
+              <h2 className="text-uppercase">Our Mission</h2>
+              <span className="text">
+                <p>
+                Technology and crowdsourced data have played a role in disaster management in multiple instances in the nations. However, disaster-related mobile apps have not yet taken off as a resource among users. The outreach of 33 freely available disaster-related mobile apps  is very limited with most of them being educational, a study has said, underscoring the potential of crowdsourcing through Global Positioning System (GPS)-enabled mobile apps in disasters.
+                </p>
+                <p>
+                The lack of data in the context of disaster resilience-that can be plugged through app-based crowdsourcing-is in sharp contrast to the smartphone and internet usage boom in the country, researchers said.{" "}
+                  <a
+                    href="http://blog.rarecarat.com/post/my-engagement-ring-story"
+                    target="_blank"
+                  >
+                    here
+                  </a>
+                  .
+                </p>
+                <p>
+                “Our results show that there is a huge potential in India to tap into app-based crowdsourcing for disaster management. By crowdsourcing, we mean communities voluntarily providing real-time data{" "}
+                  <a
+                    href="http://blog.rarecarat.com/post/watson-score-how-we-re-using-data-science-and-machine-learning"
+                    target="_blank"
+                  >
+                    blog post
+                  </a>{" "}
+                  explains more.
+                </p>
+                <p>
+                Wildfire and hurricane season are here. If disaster strikes, you'll fare better if you're prepared. It's important to have essentials on hand like drinking water, nonperishable food, battery-powered flashlights and phone chargers or power banks. 
+
+               
+                </p>
+                <p>
+                What's on your phone may be just as valuable. Check out the top apps to help you before, during and after the next emergency -- whether it's a hurricane, earthquake, wildfire or flooding. Many of these Android and iOS apps work both online and off, for help during power outages or a loss of cell service.  
+                </p>
+              </span>
+            </div>
+          </div>
+        </section>
+        
+      </div>
+    </section>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </>
+  
+    </div>
+  )
+}
+
+export default About
